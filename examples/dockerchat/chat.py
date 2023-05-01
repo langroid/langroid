797c86bc473c720d9dc024b8a37cd8ace10c2f43
Prasad Chalasani <pchalasani@gmail.com>
2023-05-01 17:58:14 -0400

    cleanup dockerchat

--- examples/dockerchat/chat.py
+++ examples/dockerchat/chat.py
@@ -10,10 +10,7 @@ from llmagent.vector_store.base import VectorStoreConfig
 from llmagent.language_models.base import LLMConfig
 from llmagent.parsing.parser import ParsingConfig
 from llmagent.prompts.prompts_config import PromptsConfig
-
-import os
 from rich import print
-import warnings
 
 app = typer.Typer()
 
@@ -91,14 +88,6 @@ def chat(config: COTAgentConfig) -> None:
     ]
 
     agent = COTAgent(config, task)
-    os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
-    warnings.filterwarnings(
-        "ignore",
-        message="Token indices sequence length.*",
-        # category=UserWarning,
-        module="transformers",
-    )
     agent.start()
     while True:
         print("\n[blue]Human: ", end="")
