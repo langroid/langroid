d16a0c9b797f70ec89f39a0ba6b6dbfcbaf11a53
Prasad Chalasani <pchalasani@gmail.com>
2023-05-04 08:23:28 -0400

    +find docker repo

---
+++ llmagent/scripts/find_docker_repos.py
@@ -0,0 +1,38 @@
+from github import Github
+import fire
+from dotenv import load_dotenv
+import os
+
+
+def find_docker_repos(stars: int = 10000, k: int = 10, access_token=None):
+    load_dotenv()
+    access_token = os.getenv("GITHUB_ACCESS_TOKEN")
+    g = Github(access_token)
+
+    # Define the search query
+    query = f"language:python stars:>={stars}"
+
+    # Fetch the repositories
+    repositories = g.search_repositories(query=query)
+
+    # Filter repositories with Dockerfile in the root directory
+    filtered_repos = []
+
+    for repo in repositories:
+        if len(filtered_repos) >= k:
+            break
+
+        try:
+            dockerfile = repo.get_contents("Dockerfile")
+            if dockerfile and dockerfile.type == "file":
+                filtered_repos.append(repo)
+        except Exception:
+            pass
+
+    # Print the filtered repository names and URLs
+    for repo in filtered_repos:
+        print(f"{repo.name}: {repo.html_url}")
+
+
+if __name__ == "__main__":
+    fire.Fire(find_docker_repos)
