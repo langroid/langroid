6059fdade1ea50c4882a8bb929a8f80aa0580bd3
Prasad Chalasani <pchalasani@gmail.com>
2023-05-08 22:42:30 -0400

    json extraction, minor logging changes

---
+++ llmagent/parsing/json.py
@@ -0,0 +1,40 @@
+import regex
+import json
+from typing import List
+
+
+def is_valid_json(json_str: str) -> bool:
+    """Check if the input string is a valid JSON.
+
+    Args:
+        json_str (str): The input string to check.
+
+    Returns:
+        bool: True if the input string is a valid JSON, False otherwise.
+    """
+    try:
+        json.loads(json_str)
+        return True
+    except ValueError:
+        return False
+
+
+def extract_top_level_json(s: str) -> List[str]:
+    """Extract all top-level JSON-formatted substrings from a given string.
+
+    Args:
+        s (str): The input string to search for JSON substrings.
+
+    Returns:
+        List[str]: A list of top-level JSON-formatted substrings.
+    """
+    # Find JSON object and array candidates using regular expressions
+    json_candidates = regex.findall(r"\{(?:[^{}]|(?R))*\}", s)
+    json_candidates += regex.findall(r"\[(?:[^\[\]]|(?R))*\]", s)
+    json_candidates = [candidate.replace("'", '"') for candidate in json_candidates]
+
+    top_level_jsons = [
+        candidate for candidate in json_candidates if is_valid_json(candidate)
+    ]
+
+    return top_level_jsons
