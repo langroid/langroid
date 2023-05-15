d9920089d5fccf686905214314db019c80cf3556
Mohannad Alhanahnah <mohannad.alhanahnah@gmail.com>
2023-05-14 19:20:21 -0500

    add an arg to support exporting repos list to CSV

--- llmagent/scripts/find_docker_repos.py
+++ llmagent/scripts/find_docker_repos.py
@@ -2,9 +2,10 @@ from github import Github
 import fire
 from dotenv import load_dotenv
 import os
+import csv
 
 
-def find_docker_repos(stars: int = 10000, k: int = 10, access_token=None):
+def find_docker_repos(stars: int = 10000, k: int = 10, to_csv=False, access_token=None):
     load_dotenv()
     access_token = os.getenv("GITHUB_ACCESS_TOKEN")
     g = Github(access_token)
@@ -31,9 +32,25 @@ def find_docker_repos(stars: int = 10000, k: int = 10, access_token=None):
 
     # Print the filtered repository names and URLs
     c = 0
+    save_repos_to_csv = []
     for repo in filtered_repos:
         print(f"{c}: {repo.name}: {repo.html_url}: {repo.stargazers_count}")
+        row = [c, repo.name, repo.html_url, repo.stargazers_count]
+        save_repos_to_csv.append(row)
         c += 1
+
+    if to_csv:
+        filename = "python_repositories.csv"
+
+        with open(filename, "w") as csvfile:
+            csvwriter = csv.writer(csvfile)
+
+            # writing the header
+            csvwriter.writerow(["No.", "Repo_name", "URL", "Stars"])
+
+            # writing each repo as a separate row in the csv
+            for repo in save_repos_to_csv:
+                csvwriter.writerow(repo)
 
 
 if __name__ == "__main__":
