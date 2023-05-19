ad2581efa90c0f505a1f89c025ec18dc669cb05c
Prasad Chalasani <pchalasani@gmail.com>
2023-05-19 12:47:16 -0400

    clean up repo_loader interface (#70)

--- tests/test_repo_loader.py
+++ tests/test_repo_loader.py
@@ -1,5 +1,4 @@
 from llmagent.parsing.repo_loader import RepoLoader, RepoLoaderConfig
-import os
 import json
 
 
@@ -10,56 +9,58 @@ def test_repo_loader() -> None:
     url = "https://github.com/eugeneyan/testing-ml"
     repo_loader = RepoLoader(url, config=RepoLoaderConfig())
 
-    docs = repo_loader.load(10, depth=0, lines=20)
+    # directly create Document objects from github repo url
+    # (uses many GitHub API calls, not recommended;
+    #  use load() instead, which clones if needed, then loads from local folder)
+    docs = repo_loader.load_docs_from_github(10, depth=0, lines=20)
     assert len(docs) > 0
     assert len(docs) <= 10
     for doc in docs:
         assert len(doc.content.split("\n")) <= 20
 
-    # tree structure direct from github
-    tree = repo_loader.get_repo_structure(depth=2)
+    # tree structure direct from github; again not recommended if easy to clone.
+    tree = repo_loader.load_tree_from_github(depth=1, lines=3)
     assert len(tree) > 0
 
-    tree_with_contents = repo_loader.get_repo_structure(depth=1, lines=5)
-    assert len(tree_with_contents) > 0
+    # tree, docs from local clone (if exists, else clone first)
+    tree, docs = repo_loader.load(depth=1, lines=5)
+    assert len(tree) > 0
+    assert len(docs) > 0
+
+    # test static fn that loads from a local folder;
+    # this is a general fn that can be used to load from any folder,
+    # not necessarily a git repo, or not necessarily even code, e.g.,
+    # could be any folder of text files
+    tree, docs = RepoLoader.load_from_folder(
+        repo_loader.clone_path,
+        depth=1,
+        lines=5,
+        file_types=["md", "txt", "toml"],
+        exclude_dirs=[".git", "tests"],
+    )
+    assert len(tree) > 0
+    assert len(docs) > 0
 
     # list all names to depth 2
-    listing = repo_loader.ls(tree_with_contents, depth=2)
+    # Useful to provide LLM a listing of contents of a repo
+    listing = repo_loader.ls(tree, depth=2)
     assert len(listing) > 0
 
-    # cloning
-    repo_loader.clone()
-    assert len(os.listdir(repo_loader.clone_path)) > 0
-    # tree structure from cloned repo
-    folder_tree = repo_loader.get_folder_structure(depth=2)
-    assert len(folder_tree) > 0
-
-    folder_tree_with_contents = repo_loader.get_folder_structure(
-        repo_loader.clone_path, depth=2, lines=5
-    )
-    assert len(folder_tree_with_contents) > 0
-
     # dump to json
-    s = json.dumps(folder_tree_with_contents, indent=2)
+    s = json.dumps(tree, indent=2)
     assert len(s) > 0
 
     # select specific files
     desired = ["workflows", "Makefile", "pyproject.toml"]
-    subtree = RepoLoader.select(
-        folder_tree_with_contents,
-        names=desired,
-    )
+    subtree = RepoLoader.select(tree, names=desired)
 
     assert len(subtree["dirs"]) + len(subtree["files"]) <= 3
 
     # select non-existent files
-    subtree = RepoLoader.select(
-        folder_tree_with_contents,
-        names=["non-existent-file"],
-    )
+    subtree = RepoLoader.select(tree, names=["non-existent-file"])
 
     assert len(subtree["dirs"]) + len(subtree["files"]) == 0
 
     # list all names to depth 2
-    listing = repo_loader.ls(folder_tree_with_contents, depth=2)
+    listing = repo_loader.ls(tree, depth=2)
     assert len(listing) > 0
