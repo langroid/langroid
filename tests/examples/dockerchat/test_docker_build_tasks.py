2fd901b6ba4132ee83571f9da000c93f1f179d72
Mohannad Alhanahnah <Mohannadcse@users.noreply.github.com>
2023-05-20 11:02:38 -0500

    switched to docker SDK in validate_dockerfile and cleanup (#69)
    * switched to docker SDK in validate_dockerfile and cleanup
    
    * remove unused imports and add spinner using halo
    
    * add a test case to verify the steps of building and cleaning up
    
    * clean up repo_loader interface (#70)
    
    * repo_loader doc fix; +doc on finding docker entry pt
    
    * addressed feedback on dependency and other issues
    
    * add docker pkg using poetry cmd
    
    * simplify message JSON instructions (#71)
    
    * chat_agent: + clear_history, reformat methods
    
    * tweaks on messages: single JSON instruction,
    rather than one per msg class
    
    * handle docker API exception and text wrapping
    
    * simplify AgentMessage; AskURLMessage (#72)
    
    * * simplify AgentMessage class, instructions
    * all tests use real cache, not fake
    * simply some tests
    
    * dockerchat: InformUrlMessage -> AskURLMessage
    
    * lints
    
    ---------
    
    Co-authored-by: Prasad Chalasani <pchalasani@gmail.com>

---
+++ tests/examples/dockerchat/test_docker_build_tasks.py
@@ -0,0 +1,86 @@
+from llmagent.agent.base import AgentConfig
+from examples.dockerchat.docker_chat_agent import DockerChatAgent
+from examples.dockerchat.dockerchat_agent_messages import (
+    AskURLMessage,
+    ValidateDockerfileMessage,
+)
+
+from llmagent.language_models.base import LLMConfig
+from llmagent.cachedb.redis_cachedb import RedisCacheConfig
+from typing import Optional
+
+import os
+import tempfile
+
+
+cfg = AgentConfig(
+    debug=False,
+    vecdb=None,
+    llm=LLMConfig(
+        type="openai",
+        chat_model="gpt-3.5-turbo",
+        cache_config=RedisCacheConfig(fake=True),
+    ),
+)
+
+
+ASK_URL_RESPONSE = """You have not yet sent me the URL. 
+            Start by asking for the URL, then confirm the URL with me"""
+
+GOT_URL_RESPONSE = """
+Ok, confirming the URL. 
+"""
+
+
+class TestDockerChatAgent(DockerChatAgent):
+    def handle_message_fallback(self, input_str: str = "") -> Optional[str]:
+        # if URL not yet known, tell LLM to ask for it, unless this msg
+        # contains the word URL
+        if self.repo_path is None and "url" not in input_str.lower():
+            return ASK_URL_RESPONSE
+
+    def ask_url(self, msg: AskURLMessage) -> str:
+        self.repo_path = "dummy"
+        return GOT_URL_RESPONSE
+
+    def validate_dockerfile(self, msg: ValidateDockerfileMessage) -> str:
+        return super().validate_dockerfile(msg, confirm=False)
+
+
+PROPOSED_DOCKERFILE_CONTENT = """
+    FROM python:3.9
+    WORKDIR /app
+    # Copy the requirements.txt file to the container
+    COPY requirements.txt .
+"""
+
+
+def test_validate_dockerfile():
+    agent = TestDockerChatAgent(cfg)
+    temp_folder_path = tempfile.mkdtemp()
+    agent.repo_path = temp_folder_path
+
+    try:
+        vdm = ValidateDockerfileMessage()
+        vdm.proposed_dockerfile = PROPOSED_DOCKERFILE_CONTENT
+        result = agent.validate_dockerfile(vdm)
+
+        # should fail because requirements.txt doesn'r exist
+        assert "failed" in result
+
+        # We shouldn't test this assetion if there is a problem with docker API
+        if "server API" not in result:
+            # Create a requirements.txt file in the folder
+            temp_file_path = os.path.join(temp_folder_path, "requirements.txt")
+            open(temp_file_path, "a").close()
+            result = agent.validate_dockerfile(vdm)
+
+            # should succeed after creating the file
+            assert "successfully" in result
+
+    finally:
+        # Clean up - remove the temporary folder and its contents
+        for filename in os.listdir(temp_folder_path):
+            file_path = os.path.join(temp_folder_path, filename)
+            os.remove(file_path)
+        os.rmdir(temp_folder_path)
