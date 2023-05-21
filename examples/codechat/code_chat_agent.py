3d9c7e04cc63bef9eac67c571b59a2edb8819a66
Prasad Chalasani <pchalasani@gmail.com>
2023-05-21 19:40:35 -0400

    codechat refac

---
+++ examples/codechat/code_chat_agent.py
@@ -0,0 +1,104 @@
+from typing import List
+from examples.urlqa.doc_chat_agent import DocChatAgent
+from llmagent.parsing.repo_loader import RepoLoader, RepoLoaderConfig
+from llmagent.agent.base import AgentConfig
+from llmagent.vector_store.qdrantdb import QdrantDBConfig
+from llmagent.embedding_models.models import OpenAIEmbeddingsConfig
+from llmagent.vector_store.base import VectorStoreConfig
+from llmagent.language_models.openai_gpt import OpenAIGPTConfig, OpenAIChatModel
+from llmagent.parsing.parser import ParsingConfig
+from llmagent.parsing.code_parser import CodeParsingConfig
+from llmagent.prompts.prompts_config import PromptsConfig
+from llmagent.mytypes import Document
+
+import os
+from rich import print
+
+
+class CodeChatAgentConfig(AgentConfig):
+    repo_url: str = "https://github.com/eugeneyan/testing-ml"
+    gpt4: bool = False
+    cache: bool = True
+    debug: bool = False
+    stream: bool = True  # allow streaming where needed
+    max_tokens: int = 10000
+    vecdb: VectorStoreConfig = QdrantDBConfig(
+        type="qdrant",
+        collection_name="llmagent-repo",
+        storage_path=".qdrant/data/",
+        embedding=OpenAIEmbeddingsConfig(
+            model_type="openai",
+            model_name="text-embedding-ada-002",
+            dims=1536,
+        ),
+    )
+
+    llm: OpenAIGPTConfig = OpenAIGPTConfig(
+        chat_model=OpenAIChatModel.GPT3_5_TURBO,
+        use_chat_for_completion=True,
+    )
+    parsing: ParsingConfig = ParsingConfig(
+        splitter="para_sentence",
+        chunk_size=500,
+        chunk_overlap=50,
+    )
+
+    code_parsing: CodeParsingConfig = CodeParsingConfig(
+        chunk_size=200,
+        token_encoding_model="text-embedding-ada-002",
+        extensions=["py", "yml", "yaml", "sh", "md", "txt"],
+        n_similar_docs=2,
+    )
+
+    prompts: PromptsConfig = PromptsConfig(
+        max_tokens=1000,
+    )
+
+
+
+
+class CodeChatAgent(DocChatAgent):
+    """
+    Agent for chatting with a code repository.
+    """
+
+    def __init__(self, config: CodeChatAgentConfig):
+        super().__init__(config)
+        self.original_docs: List[Document] = None
+        loader = RepoLoader(self.config.repo_url, config=RepoLoaderConfig())
+        dct, documents = loader.load(depth=2, lines=100)
+        listing = [
+                      """
+                      List of ALL files and directories in this project:
+                      If a file is not in this list, then we can be sure that
+                      it is not in the repo!
+                      """
+                  ] + loader.ls(dct, depth=1)
+        listing = Document(
+            content="\n".join(listing),
+            metadata={"source": "repo_listing"},
+        )
+
+        code_docs = [
+                        doc for doc in documents if doc.metadata["language"] not in ["md", "txt"]
+                    ] + [listing]
+
+        text_docs = [doc for doc in documents if doc.metadata["language"] in ["md", "txt"]]
+
+        self.config.parsing = config.parsing
+        n_text_splits = self.ingest_docs(text_docs)
+        self.config.parsing = config.code_parsing
+        n_code_splits = self.ingest_docs(code_docs)
+
+        os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+        print(
+            f"""
+        [green]I have processed {len(documents)} files from the following GitHub Repo into 
+        {n_text_splits} text chunks and {n_code_splits} code chunks:
+        {self.config.repo_url}
+        """.strip()
+        )
+
+
+
