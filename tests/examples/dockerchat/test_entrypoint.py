d34c94e573819eecbcb9a01143c96153594e6161
Mohannad Alhanahnah <mohannad.alhanahnah@gmail.com>
2023-05-24 20:20:04 -0500

    remove unused stuff

--- tests/examples/dockerchat/test_entrypoint.py
+++ tests/examples/dockerchat/test_entrypoint.py
@@ -1,17 +1,11 @@
 from llmagent.agent.base import AgentConfig
 from llmagent.language_models.openai_gpt import OpenAIGPTConfig, OpenAIChatModel
-from examples.dockerchat.docker_chat_agent import DockerChatAgent
-from examples.dockerchat.dockerchat_agent_messages import (
-    EntryPointAndCMDMessage,
-    AskURLMessage,
-)
+from examples.dockerchat.dockerchat_agent_messages import EntryPointAndCMDMessage
 from llmagent.utils.configuration import update_global_settings, Settings, set_global
 from llmagent.agent.chat_agent import ChatAgent
 from llmagent.prompts.prompts_config import PromptsConfig
 from llmagent.parsing.parser import ParsingConfig
 from llmagent.cachedb.redis_cachedb import RedisCacheConfig
-
-from typing import Optional
 
 
 class MessageHandlingAgent(ChatAgent):
@@ -29,13 +23,6 @@ cfg = AgentConfig(
     ),
 )
 
-ASK_URL_RESPONSE = """You have not yet sent me the URL. 
-            Start by asking for the URL, then confirm the URL with me"""
-
-GOT_URL_RESPONSE = """
-Ok, confirming the URL. 
-"""
-
 FIND_ENTRYPOINT_MSG = """
 great, please tell me this --
 {
@@ -46,21 +33,6 @@ great, please tell me this --
 CODE_CHAT_RESPONSE_FIND_ENTRY = """
 The name of the main script in this repo is main.py. To run it, you can use the command python main.py
 """
-
-
-# class _TestDockerChatAgent(DockerChatAgent):
-#     def handle_message_fallback(self, input_str: str = "") -> Optional[str]:
-#         # if URL not yet known, tell LLM to ask for it, unless this msg
-#         # contains the word URL
-#         if self.repo_path is None and "url" not in input_str.lower():
-#             return ASK_URL_RESPONSE
-
-#     def ask_url(self, msg: AskURLMessage) -> str:
-#         self.repo_path = "dummy"
-#         return GOT_URL_RESPONSE
-
-
-# agent = _TestDockerChatAgent(cfg)
 
 cfg = AgentConfig(
     debug=True,
@@ -110,7 +82,7 @@ def test_llm_agent_message(test_settings: Settings):
     agent = MessageHandlingAgent(cfg)
     agent.enable_message(EntryPointAndCMDMessage)
 
-    llm_msg = agent.respond(
+    llm_msg = agent.respond_forget(
         "Start by asking me about the identifying main scripts and their argument in the repo to define the ENTRYPOINT."
     ).content
 
