68e8ac282b046d6bbdc3f3074d2b380990915e58
Prasad Chalasani <pchalasani@gmail.com>
2023-06-13 11:01:26 -0400

    dockerchat - add message validater agent (#102)
    * various diagrams in mermaid, plantuml
    
    * message.py: TOOL instruction: explicitly say TOOL:
    
    * allow null llm in agent;
    dockerchat add message_validator agent

---
+++ tests/examples/dockerchat/test_message_validator.py
@@ -0,0 +1,18 @@
+from examples.dockerchat.message_validator_agent import MessageValidatorAgent
+from llmagent.agent.chat_agent import ChatAgentConfig
+
+cfg = ChatAgentConfig(vecdb=None, llm=None, name="Validator")
+
+agent = MessageValidatorAgent(cfg)
+
+
+# strictly validate expected format TO[xyz]:...
+def test_validator():
+    response = agent.agent_response("TO:[Bob]:Hello Bob")
+    assert response is not None
+    response = agent.agent_response("TO:BOB: hi there")
+    assert response is not None
+    response = agent.agent_response("hello world")
+    assert response is not None
+    response = agent.agent_response("TO[BOB]: hi there")
+    assert response is None
