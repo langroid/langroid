bd32af7de8426f768d38299101eacdbdfa570f1f
Prasad Chalasani <pchalasani@gmail.com>
2023-06-27 17:24:08 -0400

    support for improved message validation; (#119)
    ChatDocMetaData new field: parent_responder
    ChatDocument new field: attachment

--- tests/examples/dockerchat/test_message_validator.py
+++ tests/examples/dockerchat/test_message_validator.py
@@ -8,11 +8,13 @@ agent = MessageValidatorAgent(cfg)
 
 # strictly validate expected format TO[xyz]:...
 def test_validator():
+    # various bad formats -> response
     response = agent.agent_response("TO:[Bob]:Hello Bob")
     assert response is not None
     response = agent.agent_response("TO:BOB: hi there")
     assert response is not None
     response = agent.agent_response("hello world")
     assert response is not None
+    # correct format -> no response
     response = agent.agent_response("TO[BOB]: hi there")
     assert response is None
