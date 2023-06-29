5f20e01194f0039288f0fb5b2714587880f55447
Prasad Chalasani <pchalasani@gmail.com>
2023-06-29 17:49:22 -0400

    urlqa, vectorstore enhancements (#123)

--- llmagent/vector_store/qdrantdb.py
+++ llmagent/vector_store/qdrantdb.py
@@ -31,7 +31,7 @@ class QdrantDBConfig(VectorStoreConfig):
     cloud: bool = True
     url = "https://644cabc3-4141-4734-91f2-0cc3176514d4.us-east-1-0.aws.cloud.qdrant.io:6333"
 
-    collection_name: str = "qdrant-llmagent"
+    collection_name: str | None = None
     storage_path: str = ".qdrant/data"
     embedding: EmbeddingModelsConfig = EmbeddingModelsConfig(
         model_type="openai",
@@ -64,26 +64,49 @@ class QdrantDB(VectorStore):
         # This is useful to delay creation of vecdb until we have a suitable
         # collection name (e.g. we could get it from the url or folder path).
         if config.collection_name is not None:
-            self.client.recreate_collection(
-                collection_name=config.collection_name,
-                vectors_config=VectorParams(
-                    size=self.embedding_dim,
-                    distance=Distance.COSINE,
-                ),
-            )
-            collection_info = self.client.get_collection(
-                collection_name=config.collection_name
-            )
-            assert collection_info.status == CollectionStatus.GREEN
-            assert collection_info.vectors_count == 0
-            if settings.debug:
-                level = logger.getEffectiveLevel()
-                logger.setLevel(logging.INFO)
-                logger.info(collection_info)
-                logger.setLevel(level)
+            self.create_collection(config.collection_name)
+
+    def clear_empty_collections(self) -> int:
+        coll_names = self.list_collections()
+        n_deletes = 0
+        for name in coll_names:
+            info = self.client.get_collection(collection_name=name)
+            if info.points_count == 0:
+                n_deletes += 1
+                self.client.delete_collection(collection_name=name)
+        return n_deletes
+
+    def list_collections(self) -> List[str]:
+        colls = list(self.client.get_collections())[0][1]
+        return [coll.name for coll in colls]
+
+    def set_collection(self, collection_name: str) -> None:
+        self.config.collection_name = collection_name
+        if collection_name not in self.list_collections():
+            self.create_collection(collection_name)
+
+    def create_collection(self, collection_name: str) -> None:
+        self.config.collection_name = collection_name
+        self.client.recreate_collection(
+            collection_name=collection_name,
+            vectors_config=VectorParams(
+                size=self.embedding_dim,
+                distance=Distance.COSINE,
+            ),
+        )
+        collection_info = self.client.get_collection(collection_name=collection_name)
+        assert collection_info.status == CollectionStatus.GREEN
+        assert collection_info.vectors_count == 0
+        if settings.debug:
+            level = logger.getEffectiveLevel()
+            logger.setLevel(logging.INFO)
+            logger.info(collection_info)
+            logger.setLevel(level)
 
     def add_documents(self, documents: List[Document]) -> None:
         embedding_vecs = self.embedding_fn([doc.content for doc in documents])
+        if self.config.collection_name is None:
+            raise ValueError("No collection name set, cannot ingest docs")
         ids = [self._unique_hash_id(d) for d in documents]
         self.client.upsert(
             collection_name=self.config.collection_name,
@@ -102,10 +125,12 @@ class QdrantDB(VectorStore):
         text: str,
         k: int = 1,
         where: Optional[str] = None,
-    ) -> Optional[List[Tuple[Document, float]]]:
+    ) -> List[Tuple[Document, float]]:
         embedding = self.embedding_fn([text])[0]
         # TODO filter may not work yet
         filter = Filter() if where is None else Filter.from_json(where)  # type: ignore
+        if self.config.collection_name is None:
+            raise ValueError("No collection name set, cannot search")
         search_result: List[ScoredPoint] = self.client.search(
             collection_name=self.config.collection_name,
             query_vector=embedding,
@@ -124,7 +149,7 @@ class QdrantDB(VectorStore):
         ]
         if len(docs) == 0:
             logger.warning(f"No matches found for {text}")
-            return None
+            return []
         if settings.debug:
             logger.info(f"Found {len(docs)} matches, max score: {max(scores)}")
         doc_score_pairs = list(zip(docs, scores))
