6ad494a37121745829d75cd6c9905c0f9c81ab8d
Prasad Chalasani <pchalasani@gmail.com>
2023-07-07 13:16:18 -0400

    lints

--- examples/audience/target.py
+++ examples/audience/target.py
@@ -1,6 +1,5 @@
 from llmagent.utils.logging import setup_colored_logging
 from llmagent.agent.task import Task
-from llmagent.agent.chat_agent import ChatAgent, ChatAgentConfig
 from examples.audience.agents.segmentor import Segmentor, SegmentorConfig
 from llmagent.language_models.openai_gpt import OpenAIChatModel
 from llmagent.utils.configuration import update_global_settings, set_global, Settings
@@ -10,12 +9,12 @@ import typer
 from rich.console import Console
 from rich import print
 from rich.prompt import Prompt
-import warnings
 
 console = Console()
 app = typer.Typer()
 
 setup_colored_logging()
+
 
 def chat(config: SegmentorConfig) -> None:
     update_global_settings(config, keys=["debug", "stream", "cache"])
@@ -54,18 +53,19 @@ def chat(config: SegmentorConfig) -> None:
     )
     task.run()
 
+
 @app.command()
 def main(
-        debug: bool = typer.Option(False, "--debug", "-d", help="debug mode"),
-        gpt3: bool = typer.Option(False, "--gpt3_5", "-3", help="use gpt-3.5"),
-        gpt4: bool = typer.Option(False, "--gpt4", "-4", help="use gpt-4"),
-        fn_api: bool = typer.Option(False, "--fn_api", "-f", help="use functions api"),
-        no_stream: bool = typer.Option(False, "--nostream", "-ns", help="no streaming"),
-        nofunc: bool = typer.Option(False, "--nofunc", "-nf", help="no function_call"),
-        nocache: bool = typer.Option(False, "--nocache", "-nc", help="don't use cache"),
-        no_human: bool = typer.Option(
-            False, "--nohuman", "-nh", help="no human input (for stepping in debugger)"
-        ),
+    debug: bool = typer.Option(False, "--debug", "-d", help="debug mode"),
+    gpt3: bool = typer.Option(False, "--gpt3_5", "-3", help="use gpt-3.5"),
+    gpt4: bool = typer.Option(False, "--gpt4", "-4", help="use gpt-4"),
+    fn_api: bool = typer.Option(False, "--fn_api", "-f", help="use functions api"),
+    no_stream: bool = typer.Option(False, "--nostream", "-ns", help="no streaming"),
+    nofunc: bool = typer.Option(False, "--nofunc", "-nf", help="no function_call"),
+    nocache: bool = typer.Option(False, "--nocache", "-nc", help="don't use cache"),
+    no_human: bool = typer.Option(
+        False, "--nohuman", "-nh", help="no human input (for stepping in debugger)"
+    ),
 ) -> None:
     gpt4 = gpt4  # ignore since we default to gpt4 anyway
     config = SegmentorConfig(
