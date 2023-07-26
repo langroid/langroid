dccf0548d004ace5528ad74aa5f0082cc8465119
Mohannadcse <mohannad.alhanahnah@gmail.com>
2023-07-26 10:42:33 -0500

    fix linting

---
+++ tests/main/test_retriever_agent.py
@@ -0,0 +1,186 @@
+import os
+import warnings
+from typing import List, Sequence
+
+import pytest
+
+from langroid.agent.special.retriever_agent import (
+    RecordDoc,
+    RetrieverAgent,
+    RetrieverAgentConfig,
+)
+from langroid.agent.task import Task
+from langroid.cachedb.redis_cachedb import RedisCacheConfig
+from langroid.embedding_models.models import OpenAIEmbeddingsConfig
+from langroid.language_models.openai_gpt import OpenAIChatModel, OpenAIGPTConfig
+from langroid.mytypes import DocMetaData, Document, Entity
+from langroid.parsing.parser import ParsingConfig, Splitter
+from langroid.parsing.utils import generate_random_text
+from langroid.prompts.prompts_config import PromptsConfig
+from langroid.utils.configuration import Settings, set_global
+from langroid.utils.system import rmdir
+from langroid.vector_store.base import VectorStoreConfig
+from langroid.vector_store.qdrantdb import QdrantDBConfig
+
+storage_path = ".qdrant/testdata1"
+rmdir(storage_path)
+
+
+class _TesrRetrieverAgentConfig(RetrieverAgentConfig):
+    debug: bool = False
+    stream: bool = True  # allow streaming where needed
+    conversation_mode = True
+    vecdb: VectorStoreConfig = QdrantDBConfig(
+        type="qdrant",
+        collection_name="test-data",
+        storage_path=storage_path,
+        embedding=OpenAIEmbeddingsConfig(
+            model_type="openai",
+            model_name="text-embedding-ada-002",
+            dims=1536,
+        ),
+    )
+
+    llm: OpenAIGPTConfig = OpenAIGPTConfig(
+        stream=True,
+        cache_config=RedisCacheConfig(fake=False),
+        chat_model=OpenAIChatModel.GPT4,
+        use_chat_for_completion=True,
+    )
+
+    parsing: ParsingConfig = ParsingConfig(
+        splitter=Splitter.SIMPLE,
+        n_similar_docs=2,
+    )
+
+    prompts: PromptsConfig = PromptsConfig(
+        max_tokens=1000,
+    )
+
+
+config = _TesrRetrieverAgentConfig()
+set_global(Settings(cache=True))  # allow cacheing
+documents: List[Document] = (
+    [
+        Document(
+            content="""
+        In the year 2050, GPT10 was released. 
+        
+        In 2057, paperclips were seen all over the world. 
+        
+        Global warming was solved in 2060. 
+        
+        In 2061, the world was taken over by paperclips. 
+        
+        In 2045, the Tour de France was still going on.
+        They were still using bicycles. 
+        
+        There was one more ice age in 2040.
+        """,
+            metadata=DocMetaData(source="wikipedia", id=11),
+        ),
+        Document(
+            content="""
+        We need to promote green coding to address the Global warming issue
+
+        Big tech companies have started working on adapting green coding 
+
+        AI is raising significant concerns, but it's expected this issue will
+        be resolved in 2060
+        """,
+            metadata=DocMetaData(source="bbc", id=12),
+        ),
+        Document(
+            content="""
+        We are living in an alternate universe where Paris is the capital of England.
+        
+        The capital of England used to be London. 
+        
+        The capital of France used to be Paris.
+        
+        Charlie Chaplin was a great comedian.
+        
+        In 2050, all countries merged into Lithuania.
+        """,
+            metadata=DocMetaData(source="almanac", id=13),
+        ),
+    ]
+    + [
+        Document(
+            content=generate_random_text(5),
+            metadata={"source": "random", "id": c},
+        )
+        for c in range(10)
+    ]
+)
+
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+
+class _TestRetrieverAgent(RetrieverAgent):
+    def get_records(self) -> Sequence[RecordDoc]:
+        docs = []
+        for doc in documents:
+            docs.append(RecordDoc(content=doc.content, metadata=doc.metadata))
+        return docs
+
+
+agent = _TestRetrieverAgent(config)
+
+agent.ingest()
+
+
+warnings.filterwarnings(
+    "ignore",
+    message="Token indices sequence length.*",
+    # category=UserWarning,
+    module="transformers",
+)
+
+QUERY_EXPECTED_PAIRS = [
+    ("what happened in the year 2050?", "2050, GPT10"),
+    ("Who was Charlie Chaplin?", "comedian"),
+    ("What was the old capital of France?", "Paris"),
+    ("When was global warming solved?", "2060"),
+    ("what is the capital of England?", "Paris"),
+    ("What do we know about paperclips?", "2057, 2061"),
+    ("What is the purpose of green coding?", "address the Global warming"),
+]
+
+
+@pytest.mark.parametrize("query, expected", QUERY_EXPECTED_PAIRS)
+def test_retriever_chat_agent(test_settings: Settings, query: str, expected: str):
+    set_global(test_settings)
+    ans = agent.llm_response(query).content
+    expected = [e.strip() for e in expected.split(",")]
+    assert all([e in ans for e in expected])
+
+
+def test_get_nearest_docs():
+    query = "what is global warming"
+    ans = agent.get_nearest_docs(query)
+    # at lease doc id 11 and 12 should be retreived
+    assert len(ans) >= 2
+
+
+def test_get_relevant_docs():
+    query = "what is global warming"
+    ans = agent.get_relevant_docs(query)
+    assert len(ans) >= 2
+
+
+def test_retreiver_chat_process(test_settings: Settings):
+    set_global(test_settings)
+    task = Task(agent, restart=True)
+    task.init()
+    # LLM responds to Sys msg, initiates conv, says thank you, etc.
+    task.step()
+    for q, expected in QUERY_EXPECTED_PAIRS:
+        agent.default_human_response = q
+        task.step()  # user asks `q`
+        task.step()  # LLM answers
+        ans = task.pending_message.content
+        expected = [e.strip() for e in expected.split(",")]
+        assert all([e in ans for e in expected])
+        assert task.pending_message.metadata.sender == Entity.LLM
