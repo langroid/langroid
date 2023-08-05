552b3405b825512dd2b45df06093598d88f3edfb
Prasad Chalasani <pchalasani@gmail.com>
2023-08-04 20:18:34 -0400

    improve test_vector_stores.py

--- tests/main/test_vector_stores.py
+++ tests/main/test_vector_stores.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import List, Union
 
 import pytest
 from dotenv import load_dotenv
@@ -18,16 +18,6 @@ openai_cfg = OpenAIEmbeddingsConfig(
 
 
 def generate_vecdbs(embed_cfg: EmbeddingModelsConfig) -> VectorStore:
-    qd_dir = ".qdrant/" + embed_cfg.model_type
-    rmdir(qd_dir)
-    qd_cfg = QdrantDBConfig(
-        type="qdrant",
-        cloud=False,
-        collection_name="test-" + embed_cfg.model_type,
-        storage_path=qd_dir,
-        embedding=embed_cfg,
-    )
-
     qd_dir = ".qdrant/cloud/" + embed_cfg.model_type
     qd_cfg_cloud = QdrantDBConfig(
         type="qdrant",
@@ -46,25 +36,49 @@ def generate_vecdbs(embed_cfg: EmbeddingModelsConfig) -> VectorStore:
         embedding=embed_cfg,
     )
 
-    qd = QdrantDB(qd_cfg)
     qd_cloud = QdrantDB(qd_cfg_cloud)
     cd = ChromaDB(cd_cfg)
 
-    return [qd, qd_cloud, cd]
+    return [qd_cloud, cd]
 
 
+@pytest.mark.parametrize(
+    "query,results",
+    [
+        ("what is the capital of Belgium?", ["which city is Belgium's capital?"]),
+        ("hello", ["hello"]),
+        ("men and women over 40", ["people over 40"]),
+        ("people under 40", ["people under 40"]),
+        ("Canadian residents", ["people living in Canada"]),
+        ("People living outside Canada", ["people not living in Canada"]),
+    ],
+)
 @pytest.mark.parametrize("vecdb", generate_vecdbs(openai_cfg))
-def test_vector_stores(vecdb: Union[ChromaDB, QdrantDB]):
+def test_vector_stores(
+    vecdb: Union[ChromaDB, QdrantDB], query: str, results: List[str]
+):
     docs = [
         Document(content="hello", metadata=DocMetaData(id=1)),
-        Document(content="world", metadata=DocMetaData(id=2)),
-        Document(content="hi there", metadata=DocMetaData(id=3)),
+        Document(content="hi there", metadata=DocMetaData(id=2)),
+        Document(content="people living in Canada", metadata=DocMetaData(id=3)),
+        Document(content="people not living in Canada", metadata=DocMetaData(id=4)),
+        Document(content="people over 40", metadata=DocMetaData(id=5)),
+        Document(content="people under 40", metadata=DocMetaData(id=6)),
+        Document(content="what is the capital of France?", metadata=DocMetaData(id=7)),
+        Document(
+            content="which city is Belgium's capital?", metadata=DocMetaData(id=8)
+        ),
     ]
     vecdb.add_documents(docs)
-    docs_and_scores = vecdb.similar_texts_with_scores("hello", k=2)
-    assert set([docs_and_scores[0][0].content, docs_and_scores[1][0].content]) == set(
-        ["hello", "hi there"]
-    )
+    docs_and_scores = vecdb.similar_texts_with_scores(query, k=8)
+    # first doc should be best match
+    if isinstance(vecdb, ChromaDB):
+        # scores are (apparently) l2 distances (docs unclear), so low means close
+        matching_docs = [doc.content for doc, score in docs_and_scores if score < 0.2]
+    else:
+        # scores are cosine similarities, so high means close
+        matching_docs = [doc.content for doc, score in docs_and_scores if score > 0.8]
+    assert set(results).issubset(set(matching_docs))
     if vecdb.config.cloud:
         vecdb.delete_collection(collection_name=vecdb.config.collection_name)
     else:
