4325805e2736428ad93038eabb147bd92a526451
Prasad Chalasani <pchalasani@gmail.com>
2023-08-21 21:45:29 -0400

    +langroid/utils/globals.py: support global state

---
+++ langroid/utils/globals.py
@@ -0,0 +1,49 @@
+from typing import Any, Optional, Type, TypeVar
+
+from pydantic import BaseModel
+
+T = TypeVar("T", bound="GlobalState")
+
+
+class GlobalState(BaseModel):
+    """A base Pydantic model for global states."""
+
+    _instance: Optional["GlobalState"] = None
+
+    @classmethod
+    def get_instance(cls: Type["GlobalState"]) -> "GlobalState":
+        """
+        Get the global instance of the specific subclass.
+
+        Returns:
+            The global instance of the subclass.
+        """
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
+
+    @classmethod
+    def set_values(cls: Type[T], **kwargs) -> None:  # type: ignore
+        """
+        Set values on the global instance of the specific subclass.
+
+        Args:
+            **kwargs: The fields and their values to set.
+        """
+        instance = cls.get_instance()
+        for key, value in kwargs.items():
+            setattr(instance, key, value)
+
+    @classmethod
+    def get_value(cls: Type[T], name: str) -> Any:
+        """
+        Retrieve the value of a specific field from the global instance.
+
+        Args:
+            name (str): The name of the field to retrieve.
+
+        Returns:
+            str: The value of the specified field.
+        """
+        instance = cls.get_instance()
+        return getattr(instance, name)
