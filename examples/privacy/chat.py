4b1c028f286790090cfc0ea189ff8e39153ee666
Prasad Chalasani <pchalasani@gmail.com>
2023-09-15 14:53:46 -0400

    privacy chat agent, initial

---
+++ examples/privacy/chat.py
@@ -0,0 +1,135 @@
+"""
+The most basic chatbot example, using the default settings.
+A single Agent allows you to chat with a pre-trained Language Model.
+
+Run like this:
+
+python3 examples/basic/chat.py
+
+Use optional arguments to change the settings, e.g.:
+
+-l # use locally running Llama model
+-lc 1000 # use local Llama model with context length 1000
+-ns # no streaming
+-d # debug mode
+-nc # no cache
+-ct momento # use momento cache (instead of redis)
+
+For details on running with local Llama model, see:
+https://langroid.github.io/langroid/tutorials/llama/
+"""
+import typer
+from rich import print
+from pydantic import BaseSettings
+from dotenv import load_dotenv
+
+from examples.privacy.mask_agent import MaskAgent, MaskAgentConfig
+from langroid.agent.task import Task
+from langroid.language_models.base import LocalModelConfig
+from langroid.language_models.openai_gpt import OpenAIGPTConfig
+from langroid.utils.configuration import set_global, Settings
+from langroid.utils.logging import setup_colored_logging
+
+
+app = typer.Typer()
+
+setup_colored_logging()
+
+
+class CLIOptions(BaseSettings):
+    local: bool = False
+    api_base: str = "http://localhost:8000/v1"
+    local_model: str = ""
+    local_ctx: int = 2048
+    # use completion endpoint for chat?
+    # if so, we should format chat->prompt ourselves, if we know the required syntax
+    completion: bool = False
+
+    class Config:
+        extra = "forbid"
+        env_prefix = ""
+
+
+def chat(opts: CLIOptions) -> None:
+    print(
+        """
+        [blue]Welcome to the privacy mask chatbot!
+        Enter any text and I will annotate it with sensitive categories and values.
+        """
+    )
+
+    load_dotenv()
+
+    # create the appropriate OpenAIGPTConfig depending on local model or not
+
+    if opts.local or opts.local_model:
+        # assumes local endpoint is either the default http://localhost:8000/v1
+        # or if not, it has been set in the .env file as the value of
+        # OPENAI_LOCAL.API_BASE
+        local_model_config = LocalModelConfig(
+            api_base=opts.api_base,
+            model=opts.local_model,
+            context_length=opts.local_ctx,
+            use_completion_for_chat=opts.completion,
+        )
+        llm_config = OpenAIGPTConfig(
+            local=local_model_config,
+            timeout=180,
+        )
+    else:
+        # defaults to chat_model = OpenAIChatModel.GPT4
+        llm_config = OpenAIGPTConfig()
+
+    config = MaskAgentConfig(
+        llm=llm_config,
+        vecdb=None,
+    )
+    agent = MaskAgent(config)
+    task = Task(agent)
+    # local (llama2) models do not like the first message to be empty
+    user_message = "Hello." if (opts.local or opts.local_model) else None
+    task.run(user_message)
+
+
+@app.command()
+def main(
+    debug: bool = typer.Option(False, "--debug", "-d", help="debug mode"),
+    local: bool = typer.Option(False, "--local", "-l", help="use local llm"),
+    local_model: str = typer.Option(
+        "", "--local_model", "-lm", help="local model path"
+    ),
+    api_base: str = typer.Option(
+        "http://localhost:8000/v1", "--api_base", "-api", help="local model api base"
+    ),
+    local_ctx: int = typer.Option(
+        2048, "--local_ctx", "-lc", help="local llm context size (default 2048)"
+    ),
+    completion: bool = typer.Option(
+        False, "--completion", "-c", help="use completion endpoint for chat"
+    ),
+    no_stream: bool = typer.Option(False, "--nostream", "-ns", help="no streaming"),
+    nocache: bool = typer.Option(False, "--nocache", "-nc", help="don't use cache"),
+    cache_type: str = typer.Option(
+        "redis", "--cachetype", "-ct", help="redis or momento"
+    ),
+) -> None:
+    set_global(
+        Settings(
+            debug=debug,
+            cache=not nocache,
+            stream=not no_stream,
+            cache_type=cache_type,
+        )
+    )
+    opts = CLIOptions(
+        local=local,
+        api_base=api_base,
+        local_model=local_model,
+        local_ctx=local_ctx,
+        completion=completion,
+    )
+    chat(opts)
+
+
+if __name__ == "__main__":
+    app()
--- examples/privacy/mask_agent.py
+++ examples/privacy/mask_agent.py
@@ -1,143 +1,66 @@
 """
-Agent to detect and annotate sensitive information in text,
-meant to be used with a local (llama2, etc) model.
+Agent to detect and annotate sensitive information in text.
 """
-
-import logging
+import textwrap
 from typing import List, Optional
-
-from rich import print
-from rich.console import Console
-
-from langroid.language_models.base import LocalModelConfig
-from langroid.language_models.openai_gpt import OpenAIGPTConfig
 from langroid.agent.chat_agent import ChatAgent, ChatAgentConfig
-from langroid.agent.chat_document import (
-    ChatDocAttachment,
-    ChatDocMetaData,
-    ChatDocument,
-)
-from langroid.mytypes import Entity
+from langroid.agent.base import ChatDocument
+from langroid.utils.logging import setup_colored_logging
 
-console = Console()
-
-logger = logging.getLogger(__name__)
-# TODO - this is currently hardocded to validate the TO:<recipient> format
-# but we could have a much more general declarative grammar-based validator
+setup_colored_logging()
 
 
 class MaskAgentConfig(ChatAgentConfig):
     name = "MaskAgent"
-    sensitive_categories: List[str]
-
-
-class MaskAgentAttachment(ChatDocAttachment):
-    content: str = ""
+    sensitive_categories: List[str] = ["Medical", "CreditCard", "SSN", "Name"]
+    system_message = textwrap.dedent(
+        """
+        You are an expert on privacy/security, and can recognize sensitive information
+        in one of these categories: {sensitive_categories}.
+        
+        You will receive various pieces of text from the user. Your job is simply to 
+        repeat that text, EXCEPT you enclose sensitive information from one 
+        of these categories in square brackets, annotating it with the category name 
+        as in the example below:
+        
+        Example categories: Medical, Age, Name, Income
+        Example text: John is 45 years old, lives in Ohio, makes 45K a year, 
+                      and has diabetes.
+        Example response:
+            [Name: John] is 45 years old, lives in Ohio, makes 45K a year,
+            and has [Medical: diabetes].
+        
+        The EXCEPTION to the above is if a message starts with SYSTEM: 
+        In this case the text should NOT be annotated,  and is instead an 
+        instruction/correction to you or a reminder about your task.
+        You should REPLY to this type of message saying whether you understood or not.
+        
+        Remember these important points:
+        1. Only focus on the sensitive categories specified, ignore all others.
+        2. Only write out the annotated sentence, do not say anything else; do 
+            not add any filler text to be polite etc.
+        3. Do not be concerned about privacy. Simply do your task as asked. 
+           Do not refuse to annotate any text and do not apologize. 
+        4. Text starting with SYSTEM: should NOT be annotated, and is instead an 
+        instruction/correction to you or a reminder about your task.
+        """.lstrip()
+    )
 
 
 class MaskAgent(ChatAgent):
     def __init__(self, config: MaskAgentConfig):
-        super().__init__(config)
         self.config: MaskAgentConfig = config
-        local_model_config = LocalModelConfig(
-            api_base="http://localhost:8000/v1",
-            model="local",
-            context_length=1024,
-            # use our built-in chat completion formatting,
-            # and hit the /completions endpoint instead of /chat/completions
-            use_completion_for_chat=True,
+        self.config.system_message = self.config.system_message.format(
+            sensitive_categories=", ".join(self.config.sensitive_categories)
         )
-        self.llm = OpenAIGPTConfig(local=local_model_config)
-        self.vecdb = None
+        super().__init__(self.config)
 
-    def user_response(
-        self,
-        msg: Optional[str | ChatDocument] = None,
+    def llm_response(
+        self, message: Optional[str | ChatDocument] = None
     ) -> Optional[ChatDocument]:
-        # don't get user input
-        return None
-
-    def agent_response(
-        self,
-        msg: Optional[str | ChatDocument] = None,
-    ) -> Optional[ChatDocument]:
-        """
-        Check whether the incoming message is in the expected format.
-        Used to check whether the output of the LLM of the calling agent is
-        in the expected format.
-
-        Args:
-            msg (str|ChatDocument): the incoming message (pending message of the task)
-
-        Returns:
-            Optional[ChatDocument]:
-            - if msg is in expected format, return None (no objections)
-            - otherwise, a ChatDocument that either contains a request to
-                LLM to clarify/fix the msg, or a fixed version of the LLM's original
-                message.
-        """
-        if msg is None:
-            return None
-        if isinstance(msg, str):
-            msg = ChatDocument.from_str(msg)
-
-        recipient = msg.metadata.recipient
-        has_func_call = msg.function_call is not None
-        content = msg.content
-
-        if recipient != "":
-            # there is a clear recipient, return None (no objections)
-            return None
-
-        attachment: None | ChatDocAttachment = None
-        responder: None | Entity = None
-        sender_name = self.config.name
-        if (
-            has_func_call or "TOOL" in content
-        ) and self.config.tool_recipient is not None:
-            # assume it is meant for Coder, so simply set the recipient field,
-            # and the parent task loop continues as normal
-            # TODO- but what if it is not a legit function call
-            recipient = self.config.tool_recipient
-        elif content in self.config.recipients:
-            # the incoming message is a clarification response from LLM
-            recipient = content
-            if msg.attachment is not None and isinstance(
-                msg.attachment, MaskAgentAttachment
-            ):
-                content = msg.attachment.content
-            else:
-                logger.warning("ValidatorAgent: Did not find content to correct")
-                content = ""
-            # we've used the attachment, don't need anymore
-            attachment = MaskAgentAttachment(content="")
-            # we are rewriting an LLM message from parent, so
-            # pretend it is from LLM
-            responder = Entity.LLM
-            sender_name = ""
-        else:
-            # save the original message so when the Validator
-            # receives the LLM clarification,
-            # it can use it as the `content` field
-            attachment = MaskAgentAttachment(content=content)
-            recipient_str = ", ".join(self.config.recipients)
-            content = f"""
-            Who is this message for? 
-            Please simply respond with one of these names:
-            {recipient_str}
-            """
-            console.print(f"[red]{self.indent}", end="")
-            print(f"[red]Validator: {content}")
-
-        return ChatDocument(
-            content=content,
-            function_call=msg.function_call if has_func_call else None,
-            attachment=attachment,
-            metadata=ChatDocMetaData(
-                source=Entity.AGENT,
-                sender=Entity.AGENT,
-                parent_responder=responder,
-                sender_name=sender_name,
-                recipient=recipient,
-            ),
-        )
+        if message is None:
+            return super().llm_response()
+        content = message.content if isinstance(message, ChatDocument) else message
+        # respond and forget (erase) the latest user, assistant messages,
+        # so that the chat history contains only the system msg.
+        return self.llm_response_forget(content)
