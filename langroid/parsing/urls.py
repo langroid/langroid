c03fa5894d8e69e596ae57e2db27931ced53dd03
Prasad Chalasani <pchalasani@gmail.com>
2023-10-08 17:33:59 -0400

    crawl for urls

--- langroid/parsing/urls.py
+++ langroid/parsing/urls.py
@@ -2,6 +2,7 @@ import logging
 import os
 import tempfile
 import urllib.parse
+import urllib.robotparser
 from typing import List, Optional, Set, Tuple
 from urllib.parse import urljoin
 
@@ -11,6 +12,7 @@ from bs4 import BeautifulSoup
 from pydantic import BaseModel, HttpUrl, ValidationError, parse_obj_as
 from rich import print
 from rich.prompt import Prompt
+from trafilatura.spider import focused_crawler
 
 logger = logging.getLogger(__name__)
 
@@ -54,7 +56,7 @@ def get_user_input(msg: str, color: str = "blue") -> str:
 
 def get_list_from_user(
     prompt: str = "Enter input (type 'done' or hit return to finish)",
-    n: Optional[int] = None,
+    n: int | None = None,
 ) -> List[str]:
     """
     Prompt the user for inputs.
@@ -69,17 +71,23 @@ def get_list_from_user(
     input_set = set()
 
     # Use a while loop to continuously ask the user for URLs.
-    i = 0
-    while True:
+    for _ in range(n or 1000):
         # Prompt the user for input.
         input_str = Prompt.ask(f"[blue]{prompt}")
+
         # Check if the user wants to exit the loop.
         if input_str.lower() == "done" or input_str == "":
             break
-        input_set.add(input_str.strip())
-        i += 1
-        if i == n:
-            break
+
+        # if it is a URL, ask how many to crawl
+        if is_url(input_str):
+            url = input_str
+            input_str = Prompt.ask("[blue] How many new URLs to crawl?", default="0")
+            max_urls = int(input_str) + 1
+            tot_urls = crawl_url(url, max_urls=max_urls)
+            input_set.update(tot_urls)
+        else:
+            input_set.add(input_str.strip())
 
     return list(input_set)
 
@@ -116,6 +124,45 @@ def get_urls_and_paths(inputs: List[str]) -> Tuple[List[str], List[str]]:
             else:
                 logger.warning(f"{item} is neither a URL nor a path.")
     return urls, paths
+
+
+def crawl_url(url: str, max_urls: int = 1) -> List[str]:
+    """
+    Crawl the seed_urls and return a list of URLs to be parsed,
+    up to a maximum of `max_urls`.
+    """
+    if max_urls == 1:
+        # no need to crawl, just return the original list
+        return [url]
+
+    to_visit = None
+    known_urls = None
+
+    # Create a RobotFileParser object
+    robots = urllib.robotparser.RobotFileParser()
+    while True:
+        if known_urls is not None and len(known_urls) >= max_urls:
+            break
+        # Set the RobotFileParser object to the website's robots.txt file
+        robots.set_url(url + "/robots.txt")
+        robots.read()
+
+        if robots.can_fetch("*", url):
+            # Start or resume the crawl
+            to_visit, known_urls = focused_crawler(
+                url,
+                max_seen_urls=max_urls,
+                max_known_urls=max_urls,
+                todo=to_visit,
+                known_links=known_urls,
+                rules=robots,
+            )
+        if to_visit is None:
+            break
+    if known_urls is None:
+        return [url]
+    final_urls = [s.strip() for s in known_urls]
+    return list(final_urls)[:max_urls]
 
 
 def find_urls(
