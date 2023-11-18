283f08d315173d5d1319130313a512c972e03b14
Prasad Chalasani <pchalasani@gmail.com>
2023-11-18 14:19:30 -0500

    Openai assistant cache (#304)
    * OpenAIAssistant: Caching
    
    * test_redis_cache test deletion

--- tests/main/test_redis_cache.py
+++ tests/main/test_redis_cache.py
@@ -35,3 +35,22 @@ def test_real_store_and_retrieve(real_redis_cache):
     real_redis_cache.store(key, data)
     result = real_redis_cache.retrieve(key)
     assert result == data
+
+
+@pytest.mark.integration
+def test_key_deletion(real_redis_cache):
+    keys = [f"_test_key_{i}" for i in range(10)]
+    datas = [{f"info{i}": f"something{i}"} for i in range(10)]
+    for key, data in zip(keys, datas):
+        real_redis_cache.store(key, data)
+    assert real_redis_cache.retrieve(keys[0]) is not None
+    result = real_redis_cache.delete_keys(keys)
+    assert result is None
+    assert real_redis_cache.retrieve(keys[0]) is None
+
+    for key, data in zip(keys, datas):
+        real_redis_cache.store(key, data)
+    assert real_redis_cache.retrieve(keys[0]) is not None
+    result = real_redis_cache.delete_keys_pattern("_test_key_*")
+    assert result is None
+    assert real_redis_cache.retrieve(keys[0]) is None
