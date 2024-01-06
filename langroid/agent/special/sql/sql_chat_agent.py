df7b2d33efef0ee2c5976f944359a5da173247e3
Mohannadcse <mohannad.alhanahnah@gmail.com>
2024-01-05 18:32:50 -0600

    fix content field in sql_chat_agent.py

--- langroid/agent/special/sql/sql_chat_agent.py
+++ langroid/agent/special/sql/sql_chat_agent.py
@@ -227,8 +227,10 @@ class SQLChatAgent(ChatAgent):
         if isinstance(msg, ChatDocument) and msg.function_call is not None:
             sender_name = msg.function_call.name
 
+        content = results.content if isinstance(results, ChatDocument) else results
+
         return ChatDocument(
-            content=results,
+            content=content,
             metadata=ChatDocMetaData(
                 source=Entity.AGENT,
                 sender=Entity.AGENT,
