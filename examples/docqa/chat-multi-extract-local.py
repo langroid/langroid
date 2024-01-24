d6fb470f359a913f6ad5d22ba1621ed1107a1bf7
Prasad Chalasani <pchalasani@gmail.com>
2024-01-24 10:49:18 -0500

    +chat-multi-extract-local.py

---
+++ examples/docqa/chat-multi-extract-local.py
@@ -0,0 +1,290 @@
+"""
+Extract structured info from a commercial lease document,
+using multiple agents, powered by a weaker/local LLM, combine tools/functions and RAG.
+
+TASK:
+Given a lease document, generate the lease terms, organized into
+ a nested JSON structure defined by the Pydantic class Lease
+
+Solution with Langroid Agents and tools:
+1. QuestionGeneratorAgent: Lease JSON Spec -> list of questions to ask
+2. DocAgent (has access to the lease) -> answers to the questions using RAG
+3. LeasePresenterAgent: organizes questions + answers into
+   specified Lease JSON structure
+
+This works with a local mistral-instruct-v0.2 model. High level instructions:
+
+1. Spin up an OpenAI-compatible API for the model using oobabooga:
+(See https://github.com/oobabooga/text-generation-webui for details)
+python server.py --api --model mistral-7b-instruct-v0.2.Q8_0.gguf \
+   --model-dir lmstudio-models/TheBloke/Mistral-7B-Instruct-v0.2-GGUF --verbose
+
+This should load the model and start the API server at http://127.0.0.1:5000/v1
+(It won't explicitly say "/v1" but you have to include it in the URL below)
+
+2. Run this script
+
+python3 examples/docqa/chat-multi-extract-local.py
+        -m local/127.0.0.1:5000/v1//mistral-instruct-v0.2
+
+Notice we append the "//mistral-instruct-v0.2" at the end to ensure
+the chats are correctly formatted using the corresponding chat template.
+
+
+Other optional script args:
+-m <model_name_with_formatter_after//>, e.g. -m local/localhost:8000//mistral-instruct-v0.2
+-nc to disable cache retrieval
+-d to enable debug mode: see prompts, agent msgs etc.
+"""
+
+import typer
+from rich import print
+from pydantic import BaseModel
+from typing import Dict, List, Optional
+import json
+import os
+
+import langroid.language_models as lm
+from langroid.agent.batch import run_batch_tasks
+from langroid.agent import ChatDocument
+from langroid.mytypes import Entity
+from langroid.agent.special.doc_chat_agent import DocChatAgent, DocChatAgentConfig
+from langroid.parsing.parser import ParsingConfig
+from langroid.agent.chat_agent import ChatAgent, ChatAgentConfig
+from langroid.agent.task import Task
+from langroid.agent.tool_message import ToolMessage
+from langroid.language_models.openai_gpt import OpenAIGPTConfig
+from langroid.utils.configuration import set_global, Settings
+from langroid.utils.constants import NO_ANSWER, DONE
+from langroid.utils.pydantic_utils import flatten_pydantic_model
+
+app = typer.Typer()
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+
+class LeasePeriod(BaseModel):
+    start_date: str
+    end_date: str
+
+
+class LeaseFinancials(BaseModel):
+    monthly_rent: str
+    deposit: str
+
+
+class Lease(BaseModel):
+    """
+    Various lease terms.
+    Nested fields to make this more interesting/realistic
+    """
+
+    period: LeasePeriod
+    financials: LeaseFinancials
+    address: str
+
+
+class QuestionsTool(ToolMessage):
+    request: str = "questions_tool"
+    purpose: str = """
+    To present a list of <questions> to ask, to fill a desired JSON structure.
+    """
+    questions: List[str]
+
+
+class QuestionGeneratorAgent(ChatAgent):
+    questions_list: List[str] = []
+
+    def handle_message_fallback(
+        self, msg: str | ChatDocument
+    ) -> str | ChatDocument | None:
+        if isinstance(msg, ChatDocument) and msg.metadata.sender == Entity.LLM:
+            return """
+            You forgot to present the information in JSON format 
+            according to the `questions` tool specification,
+            or you may have used a wrong tool name or field name.
+            Try again.
+            """
+        return None
+
+    def questions_tool(self, msg: QuestionsTool) -> str:
+        # flatten the class and get field names
+        flat_mdl = flatten_pydantic_model(Lease)
+        fields = flat_mdl.__fields__.keys()
+        # field names can be like "period__start_date", so we only want the last part
+        fields = [f.split("__")[-1] for f in fields]
+        if len(msg.questions) < len(fields):
+            return f"""
+            ERROR: Expected {len(fields)} questions, but only got {len(msg.questions)}.
+            See what you may have missed and try again.
+            Hint: the required fields are {fields}
+            """
+        elif len(msg.questions) > len(fields):
+            return f"""
+            ERROR: Expected {len(fields)} questions, but got {len(msg.questions)}.
+            You generated an extra question. Try again.
+            Hint: the required fields are {fields}
+            """
+        else:
+            self.questions_list = msg.questions
+            return DONE + json.dumps(msg.questions)
+
+
+class LeaseMessage(ToolMessage):
+    """Tool/function to use to present details about a commercial lease"""
+
+    request: str = "lease_info"
+    purpose: str = "To present the <terms> of a Commercial lease."
+    terms: Lease
+
+    def handle(self) -> str:
+        print(
+            f"""
+        DONE! Successfully extracted Lease Info:
+        {self.terms}
+        """
+        )
+        return "DONE " + json.dumps(self.terms.dict())
+
+    @classmethod
+    def examples(cls) -> List["LeaseMessage"]:
+        return [
+            cls(
+                terms=Lease(
+                    period=LeasePeriod(start_date="2021-01-01", end_date="2021-12-31"),
+                    financials=LeaseFinancials(monthly_rent="$1000", deposit="$1000"),
+                    address="123 Main St, San Francisco, CA 94105",
+                ),
+                result="",
+            ),
+            cls(
+                terms=Lease(
+                    period=LeasePeriod(start_date="2021-04-01", end_date="2022-04-28"),
+                    financials=LeaseFinancials(monthly_rent="$2000", deposit="$2000"),
+                    address="456 Main St, San Francisco, CA 94111",
+                ),
+                result="",
+            ),
+        ]
+
+
+@app.command()
+def main(
+    debug: bool = typer.Option(False, "--debug", "-d", help="debug mode"),
+    model: str = typer.Option("", "--model", "-m", help="model name"),
+    nocache: bool = typer.Option(False, "--nocache", "-nc", help="don't use cache"),
+) -> None:
+    set_global(
+        Settings(
+            debug=debug,
+            cache=not nocache,
+            cache_type="fakeredis",
+        )
+    )
+    llm_cfg = OpenAIGPTConfig(
+        chat_model=model or lm.OpenAIChatModel.GPT4_TURBO,
+        chat_context_length=32000,  # adjust based on model
+        timeout=120,
+    )
+
+    ### (1) QUESTION GENERATOR
+    question_generator_agent = QuestionGeneratorAgent(
+        ChatAgentConfig(
+            llm=llm_cfg,
+            vecdb=None,
+            system_message="""
+            See the `lease_info` JSON structure below. 
+            Your ONLY task is to generate 
+            QUESTIONS corresponding to each field in the `lease_info` JSON,
+            and present these to me using the `questions_tool` in JSON format.
+            Pay attention to the format and fields in the `questions_tool` JSON.
+            """,
+        )
+    )
+    question_generator_agent.enable_message(LeaseMessage)
+    question_generator_agent.enable_message(QuestionsTool)
+    question_generator_task = Task(
+        question_generator_agent,
+        name="QuestionGeneratorAgent",
+        interactive=False,
+    )
+
+    ### (2) RAG AGENT
+    doc_agent = DocChatAgent(
+        DocChatAgentConfig(
+            llm=llm_cfg,
+            doc_paths=["examples/docqa/lease.txt"],
+            assistant_mode=True,
+            n_neighbor_chunks=2,
+            parsing=ParsingConfig(
+                chunk_size=100,
+                overlap=20,
+                n_similar_docs=3,
+                n_neighbor_ids=4,
+            ),
+            cross_encoder_reranking_model="",
+        )
+    )
+    doc_agent.vecdb.set_collection("docqa-chat-multi-extract", replace=True)
+    print("[blue]Welcome to the real-estate info-extractor!")
+    doc_agent.ingest()
+    doc_task = Task(
+        doc_agent,
+        name="DocAgent",
+        done_if_no_response=[Entity.LLM],  # done if null response from LLM
+        done_if_response=[Entity.LLM],  # done if non-null response from LLM
+        system_message="""You are an expert on Commercial Leases. 
+        You will receive a question about a Commercial 
+        Lease contract, and your job is to answer concisely in at most 2 sentences.
+        """,
+    )
+
+    ### (3) LEASE PRESENTER
+    lease_presenter = ChatAgent(
+        ChatAgentConfig(
+            llm=llm_cfg,
+            vecdb=None,
+        )
+    )
+    lease_presenter.enable_message(LeaseMessage)
+
+    lease_presenter_task = Task(
+        lease_presenter,
+        name="LeasePresenter",
+        interactive=False,  # set to True to slow it down (hit enter to progress)
+        system_message=f"""
+        The user will give you a list of Questions and Answers 
+        about a commercial lease.
+        
+        Organize this information into the `lease_info` JSON structure specified below,
+        and present it to me.        
+        """,
+    )
+
+    ### (4) Use the agents/tasks
+
+    # Lease info JSON -> Questions
+    question_generator_task.run()
+    questions = question_generator_agent.questions_list
+    print(f"found {len(questions)} questions! Now generating answers...")
+    answers = []
+    # Questions -> Answers using RAG
+    for q in questions:
+        answer = doc_task.run(q)
+        answers.append(answer.content)
+    # answers = run_batch_tasks(
+    #     doc_task,
+    #     questions,
+    # )
+    print(f"found {len(answers)} answers!")
+    q2a = dict(zip(questions, answers))
+    print(f"q2a: {q2a}")
+    questions_answers = "\n\n".join(
+        f"Question: {q}:\nAnswer: {a}" for q, a in q2a.items()
+    )
+    # Questions + Answers -> organized into nested Lease Info JSON
+    lease_presenter_task.run(questions_answers)
+
+
+if __name__ == "__main__":
+    app()
