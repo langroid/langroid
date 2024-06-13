85a186bf0f2d334befbb9718ecb4dd7f9e9a77a3
Prasad Chalasani <pchalasani@gmail.com>
2024-06-13 15:33:52 -0400

    chainlit: pin to 1.1.202; add test

---
+++ examples/chainlit/test-step-nesting.py
@@ -0,0 +1,40 @@
+"""
+Test whether the current chainlit version shows nested steps as expected.
+
+Two things to look for:
+(1) are all types of steps shown, or only type = "tool"?
+(2) when step B has parent_id pointing to Step A, we want to see Step B shown:
+    - nested under Step A
+    - shown in a chronologically correct order, i.e. if Step A says "hello",
+        then calls Step B, then step B should be shown AFTER the "hello" message from A.
+
+(1) is fine in chainlit 1.1.202, i.e. all steps are shown whether tools or not
+    but in 1.1.300, only type = "tool" steps are shown.
+(2) is broken in 1.1.202 -- the sub-step is correctly nested BUT always shows up
+    at the TOP, and can look very unintuitive, as this example shows.
+"""
+
+import chainlit as cl
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    a_step = cl.Step(name="A", type="tool")
+    a_step.output = "asking B"
+    await a_step.send()
+
+    b_step = cl.Step(
+        name="B",
+        parent_id=a_step.id,
+        type="tool",
+    )
+    b_step.output = "asking C"
+    await b_step.send()
+
+    c_step = cl.Step(
+        name="C",
+        parent_id=b_step.id,
+        type="tool",
+    )
+    c_step.output = "C answered!"
+    await c_step.send()
