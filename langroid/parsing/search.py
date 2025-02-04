3f157173252d215082896f3035d7e395b457f16a
kagia <benjaminkagia@gmail.com>
2025-02-04 19:45:45 +0100

    fix/qualify-nltk-models
    when using find nltk requires <subdirectory>/<model> format.
    when downloading nltk requires <model> format and uses this
    xml to detect subdir https://raw.githubusercontent.com/nltk/nltk_data/gh-pages/index.xml
    this means that previously we were getting cache miss since we only used <model>
    .

--- langroid/parsing/search.py
+++ langroid/parsing/search.py
@@ -118,7 +118,7 @@ def preprocess_text(text: str) -> str:
         str: The preprocessed text.
     """
     # Ensure the NLTK resources are available
-    for resource in ["punkt", "wordnet", "stopwords"]:
+    for resource in ["tokenizers/punkt", "corpora/wordnet", "corpora/stopwords"]:
         download_nltk_resource(resource)
 
     # Lowercase the text
